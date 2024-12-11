8826b364810a7116b5f4944362412dc531523f51
Simon Gerstmeier <simon@gerstmeier.dev>
2024-12-11 19:29:22 +0100

    Fixed the server's access rules to allow the creation of new tournament organizer accounts.

--- migrations/1711458535_collections_snapshot.go
+++ migrations/1711458535_collections_snapshot.go
@@ -715,6 +715,26 @@ func init() {
 						"presentable": false,
 						"system": false,
 						"type": "autodate"
+					},
+					{
+						"hidden": false,
+						"id": "file376926767",
+						"maxSelect": 1,
+						"maxSize": 0,
+						"mimeTypes": [
+							"image/jpeg",
+							"image/png",
+							"image/svg+xml",
+							"image/gif",
+							"image/webp"
+						],
+						"name": "avatar",
+						"presentable": false,
+						"protected": false,
+						"required": false,
+						"system": false,
+						"thumbs": null,
+						"type": "file"
 					}
 				],
 				"fileToken": {
@@ -794,7 +814,7 @@ func init() {
 					"body": "<p>Hello,</p>\n<p>Click on the button below to confirm your new email address.</p>\n<p>\n  <a class=\"btn\" href=\"{APP_URL}/_/#/auth/confirm-email-change/{TOKEN}\" target=\"_blank\" rel=\"noopener\">Confirm new email</a>\n</p>\n<p><i>If you didn't ask to change your email address, you can ignore this email.</i></p>\n<p>\n  Thanks,<br/>\n  {APP_NAME} team\n</p>",
 					"subject": "Confirm your {APP_NAME} new email address"
 				},
-				"createRule": null,
+				"createRule": "",
 				"deleteRule": null,
 				"emailChangeToken": {
 					"duration": 1800
