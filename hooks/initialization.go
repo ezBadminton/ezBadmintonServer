5a88397b8e0f4237e997f6714b0b832224cce68f
Simon Gerstmeier <simon@gerstmeier.dev>
2025-04-04 12:35:34 +0200

    Separated the hook binding and other initialization to an extra function for access in tests

---
+++ hooks/initialization.go
@@ -0,0 +1,42 @@
+package hooks
+
+import (
+	"github.com/ezBadminton/ezBadmintonServer/api"
+	"github.com/ezBadminton/ezBadmintonServer/store"
+	"github.com/ezBadminton/ezBadmintonServer/tops"
+	"github.com/pocketbase/pocketbase/core"
+)
+
+func InitHooksAndApi(app core.App) {
+	api.InitRootApiRoute(app)
+
+	api.BindOrganizerRegistrationHooks(app)
+	api.BindClubHooks(app)
+	api.BindRegistrationHooks(app)
+	api.BindTournamentPlanHooks(app)
+	api.BindTournamentMatchHooks(app)
+	api.BindDrawHooks(app)
+	api.BindPlayerStatusHooks(app)
+	api.BindCourtHooks(app)
+	api.BindMatchHooks(app)
+	api.BindStartStopHooks(app)
+	api.BindTieBreakerHooks(app)
+	api.BindMatchScheduleHooks(app)
+	api.BindEventSettingsHooks(app)
+	api.BindCategoryHooks(app)
+	api.BindTournamentModeSettingsHooks(app)
+	api.BindCompetitionHooks(app)
+
+	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
+		if err := store.InitStores(e.App); err != nil {
+			return err
+		}
+		tops.InitTournamentOperations(e.App)
+
+		return e.Next()
+	})
+
+	app.Cron().Add("organizer_ping", "*/4 * * * *", func() {
+		tops.PingOrganizerClients(app)
+	})
+}
--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/ezBadminton/ezBadmintonServer/api"
 	watchers "github.com/ezBadminton/ezBadmintonServer/client_watchers"
-	"github.com/ezBadminton/ezBadmintonServer/store"
 
 	_ "github.com/ezBadminton/ezBadmintonServer/migrations"
 
-	"github.com/ezBadminton/ezBadmintonServer/tops"
+	"github.com/ezBadminton/ezBadmintonServer/hooks"
 
 	"github.com/pocketbase/pocketbase"
 	"github.com/pocketbase/pocketbase/core"
@@ -26,46 +24,18 @@ func main() {
 		false,
 		"with this option the server terminates itself when the client exits. Only works when the server is a child process of the client.",
 	)
-
-	api.InitRootApiRoute(app)
-
-	api.BindOrganizerRegistrationHooks(app)
-	api.BindClubHooks(app)
-	api.BindRegistrationHooks(app)
-	api.BindTournamentPlanHooks(app)
-	api.BindTournamentMatchHooks(app)
-	api.BindDrawHooks(app)
-	api.BindPlayerStatusHooks(app)
-	api.BindCourtHooks(app)
-	api.BindMatchHooks(app)
-	api.BindStartStopHooks(app)
-	api.BindTieBreakerHooks(app)
-	api.BindMatchScheduleHooks(app)
-	api.BindEventSettingsHooks(app)
-	api.BindCategoryHooks(app)
-	api.BindTournamentModeSettingsHooks(app)
-	api.BindCompetitionHooks(app)
-
 	app.OnServe().BindFunc(func(e *core.ServeEvent) error {
 		if watchClient {
 			watchers.WatchClientForExit()
 		}
-
-		if err := store.InitStores(e.App); err != nil {
-			return err
-		}
-		tops.InitTournamentOperations(e.App)
-
 		return e.Next()
-	})
-
-	app.Cron().Add("organizer_ping", "*/4 * * * *", func() {
-		tops.PingOrganizerClients(app)
 	})
 
 	migratecmd.MustRegister(app, app.RootCmd, migratecmd.Config{
 		Automigrate: false,
 	})
+
+	hooks.InitHooksAndApi(app)
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
