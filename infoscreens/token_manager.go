5426f55ef4df9106f6ebd3110a25669464966e93
Simon Gerstmeier <simon@gerstmeier.dev>
2025-06-11 21:59:56 +0200

    Added a grace period of 10 seconds to the controller init tokens. Reduced the token refresh time to 90 seconds.

--- infoscreens/token_manager.go
+++ infoscreens/token_manager.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"errors"
+	"maps"
 	"sync"
 	"time"
 
@@ -37,15 +38,24 @@ type TokenManager struct {
 	refreshInterval        time.Duration
 	tokenLifetime          time.Duration
 	activeControllerTokens map[string]chan any
-	mu                     sync.Mutex
+	// When the init tokens get refreshed the old ones get a short
+	// grace period where both the old and the new one can be used.
+	// This is for avoiding situations where someone scans a
+	// QR code with a controller token right before it gets
+	// refreshed, leaving the scanner with an invalid token.
+	gracePeriodTokens map[string]string
+	gracePeriod       time.Duration
+	mu                sync.Mutex
 }
 
 func InitTokenManager(app core.App) {
 	Tokens = &TokenManager{
 		app:                    app,
-		refreshInterval:        2 * time.Minute,
+		refreshInterval:        90 * time.Second,
 		tokenLifetime:          5 * time.Minute,
 		activeControllerTokens: make(map[string]chan any),
+		gracePeriodTokens:      make(map[string]string),
+		gracePeriod:            10 * time.Second,
 	}
 
 	infoscreenUserCName := CName[InfoscreenUser]()
@@ -88,10 +98,19 @@ func (m *TokenManager) startControllerTokenTimer(token string, cancel chan any) 
 }
 
 func (m *TokenManager) ValidateToken(token string) (*InfoscreenUser, error) {
+	lookupToken := token
+
+	m.mu.Lock()
+	graceToken, isGraceToken := m.gracePeriodTokens[lookupToken]
+	if isGraceToken {
+		lookupToken = graceToken
+	}
+	m.mu.Unlock()
+
 	infoscreenUserCName := CName[InfoscreenUser]()
 	infoscreenUserRecord := &core.Record{}
 	err := m.app.RecordQuery(infoscreenUserCName).
-		AndWhere(dbx.HashExp{"initToken": token}).
+		AndWhere(dbx.HashExp{"initToken": lookupToken}).
 		OrWhere(dbx.HashExp{"controllerToken": token}).
 		Limit(1).
 		One(infoscreenUserRecord)
@@ -104,8 +123,8 @@ func (m *TokenManager) ValidateToken(token string) (*InfoscreenUser, error) {
 
 	infoscreenUser, _ := WrapRecord[InfoscreenUser](infoscreenUserRecord)
 
-	if infoscreenUser.InitToken() == token {
-		if err := m.useInitToken(infoscreenUser); err != nil {
+	if infoscreenUser.InitToken() == token || isGraceToken {
+		if err := m.useInitToken(infoscreenUser, token); err != nil {
 			return nil, err
 		}
 	}
@@ -118,10 +137,18 @@ func (m *TokenManager) invalidateControllerToken(token string) {
 	delete(m.activeControllerTokens, token)
 	m.mu.Unlock()
 
-	infoscreenUser, err := m.ValidateToken(token)
-	if err != nil {
+	infoscreenUserCName := CName[InfoscreenUser]()
+	infoscreenUserRecord := &core.Record{}
+	err := m.app.RecordQuery(infoscreenUserCName).
+		AndWhere(dbx.HashExp{"controllerToken": token}).
+		Limit(1).
+		One(infoscreenUserRecord)
+
+	if err != nil || infoscreenUserRecord.Id == "" {
 		return
 	}
+
+	infoscreenUser, _ := WrapRecord[InfoscreenUser](infoscreenUserRecord)
 	infoscreenUser.SetControllerToken("")
 	m.app.Save(infoscreenUser)
 }
@@ -130,18 +157,22 @@ func (m *TokenManager) refreshInitTokens() {
 	infoscreenUserStore, _ := store.FindRecordStore[InfoscreenUser]()
 	infoscreenUsers := infoscreenUserStore.RecordList
 
+	oldToNewTokens := make(map[string]string)
+
 	for _, infoscreenUser := range infoscreenUsers {
+		old := infoscreenUser.InitToken()
+		new := newToken()
 		infoscreenUser = Clone(infoscreenUser)
-		infoscreenUser.SetInitToken(newToken())
+		infoscreenUser.SetInitToken(new)
+		oldToNewTokens[old] = new
 		m.app.Save(infoscreenUser)
 	}
+	m.startTokenGracePeriod(oldToNewTokens)
 }
 
 // Moves the init token to being the controller token and
 // creates a new init token
-func (m *TokenManager) useInitToken(infoscreenUser *InfoscreenUser) error {
-	initToken := infoscreenUser.InitToken()
-
+func (m *TokenManager) useInitToken(infoscreenUser *InfoscreenUser, initToken string) error {
 	m.mu.Lock()
 	_, ok := m.activeControllerTokens[initToken]
 	if ok {
@@ -180,6 +211,22 @@ func (m *TokenManager) handleInfoscreenUserDeletion(e *core.RecordEvent) error {
 		m.mu.Unlock()
 	}
 	return e.Next()
+}
+
+// The tokens map maps old token (that goes into grace period) -> new token
+func (m *TokenManager) startTokenGracePeriod(tokens map[string]string) {
+	m.mu.Lock()
+	maps.Copy(m.gracePeriodTokens, tokens)
+	m.mu.Unlock()
+	go func() {
+		timer := time.NewTimer(m.gracePeriod)
+		<-timer.C
+		m.mu.Lock()
+		for token := range tokens {
+			delete(m.gracePeriodTokens, token)
+		}
+		m.mu.Unlock()
+	}()
 }
 
 func newToken() string {
